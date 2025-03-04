87f4a744368b5001fef7fcb405e0fa2d89e10497
RxHuL12 <qpskzm2@gmail.com>
2025-03-05 04:07:10 +0530

    DSA

---
+++ BinaryTrees/Symmetric_Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        int fun(TreeNode* p, TreeNode* q) {
+            if (!p && !q) {
+                return 1;
+            }
+            if (p && !q || !p && q) {
+                return 0;
+            }
+            if (p && q && p->val != q->val) {
+                return 0;
+            }
+            int leftSide = fun(p->left, q->right);
+            int rightSide = fun(p->right, q->left);
+            return leftSide && rightSide;
+        }
+        bool isSymmetric(TreeNode* root) {
+            int ans = fun(root->left, root->right);
+            return ans;
+        }
+    };
