e8929a1564fd2a80d25a4c3aa2aa2fa41a04ad0c
RxHuL12 <qpskzm2@gmail.com>
2025-03-06 02:10:53 +0530

    DSA

---
+++ BinaryTrees/Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        TreeNode* fun(vector<int>& preorder, vector<int>& inorder,
+                      unordered_map<int, int>& mp, int& idx, int start, int end) {
+    
+            if (start > end) {
+                return NULL;
+            }
+            int value = preorder[idx];
+            TreeNode* temp = new TreeNode(value);
+            idx++;
+    
+            int cur_idx = mp[value];
+    
+            temp->left = fun(preorder, inorder, mp, idx, start, cur_idx - 1);
+            temp->right = fun(preorder, inorder, mp, idx, cur_idx + 1, end);
+    
+            return temp;
+        }
+        TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+            unordered_map<int, int> mp;
+            int idx = 0;
+            for (int i = 0; i < inorder.size(); i++) {
+                mp[inorder[i]] = i;
+            }
+            TreeNode* root = fun(preorder, inorder, mp, idx, 0, inorder.size() - 1);
+            return root;
+        }
+    };
