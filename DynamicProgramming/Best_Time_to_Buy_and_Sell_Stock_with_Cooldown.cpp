7cf820e4e6a4307b268b5d61787fd6986afa7712
RxHuL12 <qpskzm2@gmail.com>
2025-04-22 02:55:17 +0530

    DSA

---
+++ DynamicProgramming/Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int fun(vector<int>& prices, vector<vector<int>>& dp, int idx, int flag) {
+            if (idx >= prices.size()) {
+                return 0;
+            }
+            if (dp[idx][flag] != -1) {
+                return dp[idx][flag];
+            }
+            if (flag == 0) {
+                int buy = fun(prices, dp, idx + 1, !flag) - prices[idx];
+                int not_buy = fun(prices, dp, idx + 1, flag);
+                return dp[idx][flag] = max(buy, not_buy);
+            } else {
+                int sell = fun(prices, dp, idx + 2, !flag) + prices[idx];
+                int not_sell = fun(prices, dp, idx + 1, flag);
+                return dp[idx][flag] = max(sell, not_sell);
+            }
+        }
+        int maxProfit(vector<int>& prices) {
+            vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+            int ans = fun(prices, dp, 0, 0);
+            return (ans < 0) ? 0 : ans;
+        }
+    };
